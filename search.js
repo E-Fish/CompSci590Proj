fcdb898f1b4987c2eec5a1c76059bdc4f20653ba
NicoBerger2 <131185451+NicoBerger2@users.noreply.github.com>
2023-05-19 09:05:32 -0400

    Update search.js

--- search.js
+++ search.js
@@ -4,51 +4,55 @@ import { mongoClient, MONGODB_COLLECTION } from "./util"
 
 const app = express()
 
+let input = document.getElementById("coursesearch");
 let collection = document.getElementsByClassName("courseCard");
 
 app.use(cors({ credentials: true, origin: "http://localhost:4000" }))
 
 app.get("/search", async (req, res) => {
-    const searchQuery = req.query.query
-    const country = req.query.country
+  const searchQuery = input;
 
-    if (!searchQuery || searchQuery.length < 2) {
-        res.json([])
-        return
+
+  if (!searchQuery || searchQuery.length < 2) {
+    res.json([])
+    return
+  }
+
+  const db = mongoClient.db("classes")
+  const collection = db.collection("courseCard")
+
+  const filter = {
+    $text: {
+      $search: searchQuery,
+      $caseSensitive: false,
+      $diacriticSensitive: false
     }
-
-    const db = mongoClient.db("classes")
-    const collection = db.collection(MONGODB_COLLECTION)
-
-    const filter = {
-        $text: {
-            $search: searchQuery,
-            $caseSensitive: false,
-            $diacriticSensitive: false
-        }
-    }
+  }
 
 })
 
 async function main() {
-    try {
-        await mongoClient.connect()
+  try {
+    await mongoClient.connect()
 
-        const db = mongoClient.db("classes")
-        const collection = db.collection(MONGODB_COLLECTION)
+    const db = mongoClient.db("classes")
+    const collection = db.collection(MONGODB_COLLECTION)
 
-        await collection.createIndexes([
-        { name: "course_no", key: { dept: "text"} }
+   
+    await collection.createIndexes([
+        db.classes.createIndex({ course_no: "text"})
+
     ])
 
-    } catch (err) {
-        console.log(err)
-    }
+    
+  } catch (err) {
+    console.log(err)
+  }
 
-    process.on("SIGTERM", async () => {
-        await mongoClient.close()
-        process.exit(0)
-    })
+  process.on("SIGTERM", async () => {
+    await mongoClient.close()
+    process.exit(0)
+  })
 }
 
 main()
