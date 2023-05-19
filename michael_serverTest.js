33082b0c35f9ae765745a60a1765e0a4bff7db3a
NicoBerger2 <131185451+NicoBerger2@users.noreply.github.com>
2023-05-19 09:45:46 -0400

    Update michael_serverTest.js

--- michael_serverTest.js
+++ michael_serverTest.js
@@ -1,8 +1,13 @@
+const express = require('express');
+const app = express();
 const { MongoClient } = require("mongodb");
+
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri =
   "mongodb+srv://cSc590:ehHsu3NViHPgLvWX@cluster0.ueyan8p.mongodb.net/test";
 const client = new MongoClient(uri);
+
+let username = document.querySelector('input');
 
 async function run() {
   try {
@@ -12,14 +17,19 @@ async function run() {
     const coll = db.collection("ratings_reviews");
 
     // insert code goes here
-    const docs = [
+    app.post("/", function(req, res) {
+        username: req.body.username;
+    })
+    /*const docs = [
       {
+        username: username,
         rating: "5",
         review: "GReat!",
         short_title: "SOME ART CLASS",
         course_no: "ART*200"
       }
-    ]
+    ]*/
+
 
     const result = await coll.insertMany(docs);
 
@@ -30,5 +40,8 @@ async function run() {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
+}
+app.listen(3000,function()) {
+    console.log("server is running in 3000");
 }
 run().catch(console.dir);
