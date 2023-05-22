741be4cc10bfac0240a5d7e2ba5075fc2604a0a8
JonnyChen2024 <99998530+JonnyChen2024@users.noreply.github.com>
2023-05-22 11:22:03 -0400

    Update addcoursereviews.js

--- addcoursereviews.js
+++ addcoursereviews.js
@@ -52,7 +52,8 @@ async function addcoursereviews()
         Review.innerText = body;
 
         //adding rating to postCard
-        let Rating = document.createElement("ratingb");
+        let Rating = document.createElement("div");
+        Rating.setAttribute("class", "ratingc");
         Rating.innerHTML = '<input type="radio" id="star5" name="rating" value="5" /><label for="star5"></label> <input type="radio" id="star4" name="rating" value="4" /><label for="star4"></label> <input type="radio" id="star3" name="rating" value="3" /><label for="star3"></label> <input type="radio" id="star2" name="rating" value="2" /><label for="star2"></label> <input type="radio" id="star1" name="rating" value="1" /><label for="star1"></label>'
 
 
