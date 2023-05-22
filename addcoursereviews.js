bc8a79c69a568f878213b7521936e4ae1e9eaa0e
JonnyChen2024 <99998530+JonnyChen2024@users.noreply.github.com>
2023-05-22 11:01:16 -0400

    Update addcoursereviews.js

--- addcoursereviews.js
+++ addcoursereviews.js
@@ -37,21 +37,24 @@ async function addcoursereviews()
         mycard.setAttribute("class", "postCard");
        
         //creates the <h1> tag for the User and Date
-        let UserDate = document.createElement("h1");
+        let UserDate = document.createElement("div");
+        UserDate.setAttribute("class", "user")
         UserDate.innerText = user + " ( " + date + " )";
        
         //creates the <p> tag for the post body
-        let Title = document.createElement("p");
+        let Title = document.createElement("div");
+        Title.setAttribute("class", "body")
         Title.innerText = title;
     
         //creates the <h3> tag for the reviewer name and date
-        let Review = document.createElement("h3");
+        let Review = document.createElement("div");
+        Review.setAttribute("class", "review")
         Review.innerText = body;
 
         //adding rating to postCard
         let Rating = document.createElement("ratingb");
         Rating.innerHTML = '<input type="radio" id="star5" name="rating" value="5" /><label for="star5"></label> <input type="radio" id="star4" name="rating" value="4" /><label for="star4"></label> <input type="radio" id="star3" name="rating" value="3" /><label for="star3"></label> <input type="radio" id="star2" name="rating" value="2" /><label for="star2"></label> <input type="radio" id="star1" name="rating" value="1" /><label for="star1"></label>'
-        
+
 
         mycard.appendChild(UserDate);
         mycard.appendChild(Title);
