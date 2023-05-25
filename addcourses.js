c0fe9fc15c815fb8ba2a152bce78d83987debac8
ari-tistic <131185548+ari-tistic@users.noreply.github.com>
2023-05-25 12:54:00 -0400

    Update addcourses.js

--- addcourses.js
+++ addcourses.js
@@ -15,27 +15,48 @@ async function addcourses()
         let index = courses[i].indexCourse; //The course index
         let name = courses[i].name; //The class name
         let title = courses[i].title; //The class title
-        // Will use line below eventually
-        // let rating = courses[i].rating;
-        // For now just 3 stars
-        let rating = 3;
+        let description = courses[i].description;
+
+         //lastUser, lastDate, and averageRating should come from database
+        let lastUser = "lionrampant" + cnt.toString();
+        let lastDate = "2023-05-25";
+        let averageRating = 1.0 + Math.random()*4; //generate random number from 1 to 5
+        let rating = Math.round(averageRating);
        
         //This creates a new course card
         let mycard = document.createElement("div");
         mycard.setAttribute("class", "courseCard");
        
         //This creates the <h1> tag for the course name, e.g. "ANT500"
-        let h1 = document.createElement("h1");
-        h1.innerText = name;
+        let myname = document.createElement("h1");
+        myname.setAttribute("id", "courseName" + index);
+        myname.setAttribute("class", "courseNames");
+        myname.innerText = name;
        
         //This creates the <p> tag for the course title, e.g. "Cultural Anthropology"
-        let p = document.createElement("p");
-        p.innerText = title;
+        let mytitle = document.createElement("p");
+        mytitle.setAttribute("id", "courseTitle" + index);
+        mytitle.setAttribute("class", "courseTitles");
+        mytitle.innerText = title;
+
+        //Create a tag for the course description but don't display it
+        let mydesc = document.createElement("p");
+        mydesc.setAttribute("id", "courseDescription"+index);
+        mydesc.innerText = description;
+        mydesc.style.display = "none"; //doesn't take up space and not shown
+       
+        //Create a tag for the average rating but don't display it
+        let myrating = document.createElement("p");
+        myrating.setAttribute("id", "courseRating"+index);
+        myrating.innerText = rating;
+        myrating.style.display = "none"; //doesn't take up space and not shown
        
         //This creates the <h3> tag for the last review date/time and last reviewer
         //We will fill this in AFTER we have created all courses, so for now put in dummy information
-        let h3 = document.createElement("h3");
-        h3.innerText = "Last review:" + " lionrampant" + cnt.toString() + "  (2023-05-11)";
+        let mylast = document.createElement("h3");
+        mylast.setAttribute("id", "courseLastReview" + index);
+        mylast.setAttribute("class", "courseLastReviews");
+        mylast.innerText = "Last review: " + lastUser +  "(" + lastDate + ")";
 
         //adding rating to postCard using a string "starString"
         let Stars = document.createElement("div");
@@ -48,9 +69,11 @@ async function addcourses()
         }
         Stars.innerHTML = starString;
         
-        mycard.appendChild(h1);
-        mycard.appendChild(p);
-        mycard.appendChild(h3);
+        mycard.appendChild(myname);
+        mycard.appendChild(mytitle);
+        mycard.appendChild(mydesc);
+        mycard.appendChild(myrating);
+        mycard.appendChild(mylast);
         mycard.appendChild(Stars);
        
         mycard.addEventListener("click", function() { clickedoncourse(index); });
