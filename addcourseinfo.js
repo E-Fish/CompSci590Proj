ba36616d8274961486acd7557f2e7f9a166e7047
JonnyChen2024 <99998530+JonnyChen2024@users.noreply.github.com>
2023-05-28 15:17:52 -0400

    Update addcourseinfo.js

--- addcourseinfo.js
+++ addcourseinfo.js
@@ -1,57 +1,44 @@
-//EXTREMELY PRELIMIARY JS FILE. NOTHING WORKS.
-
-async function addRating(){
+async function addcourseinfo()
+{
+    let selected = localStorage.getItem("classIndex");
+    console.log("Trying to fetch posts")
     let response = await fetch("https://raw.githubusercontent.com/E-Fish/CompSci590Proj/main/static/courses.json");
+    let courses = await response.json(); 
 
-    let averageRating = 1.0 + Math.random()*4; //generate random number from 1 to 5
-    let rating = Math.round(averageRating);
+    let divTag = document.getElementById("allInfo");
 
-    for (let i in courses) { 
+    let cnt = 0;
 
-        let Stars = document.createElement("div");
-        let starString = "";
-        for (let i = 0; i < rating; i++) {
-            starString += "<i class = 'fa fa-star checked' style='font-size:30px'></i>"
-        }
-        for (let i = rating; i<5; i++) {
-            starString += "<i class = 'fa fa-star unchecked' style='font-size:30px'></i>"
-        }
-        Stars.innerHTML = starString;
+    for (let i in courses){
+        let index = courses[i].indexCourse;
+        
+        if (index != selected) continue;
 
-        return Stars;
-    }
+        let name = courses[i].name; //name of course ie ANT500
+        let title = courses[i].title; //title of course ie Cultural Anthropology
+        let description = courses[i].desc; //description of course from course book
+        let rating = courses[i].rating; //rating of course (not yet in courses.json file)
 
-}
+        let myinfo = document.createElement ("div")
+        myinfo.setAtrtribute("class", "h4");
 
-// No subject in courses.json yet
-async function addSubject() {
-    let response = await fetch("https://raw.githubusercontent.com/E-Fish/CompSci590Proj/main/static/courses.json");
+        let courseName = document.createElement("div");
+        courseName.setAttribute("class", "h4");
+        courseName.innerText = name;
 
-    
-    for (let i in courses) {       
-        let subject = courses[i].subject;
+        let courseTitle = document.createElement("div");
+        courseTitle.setAttribute("class", "h4");
+        courseTitle.innerText = title;
 
-        //Create a tag for the Subject
-        let mysubject = document.createElement("p");
-        mysubject.setAttribute("id", "courseSubject"+index);
-        mysubject.innerText = subject;
+        let courseDescription = document.createElement("div");
+        courseDescription.setAttribute("class", "h4");
+        courseDescription.innerText = description;
 
-        return mysubject;
-    }
-}
+        myinfo.appendChild(courseName + " : " + courseTitle);
+        myinfo.appendChild("Description: " + courseDescription)
+        divTag.appendChild(myinfo);
 
-async function addDescription() {
-    let response = await fetch("https://raw.githubusercontent.com/E-Fish/CompSci590Proj/main/static/courses.json");
-    
-    for (let i in courses) {
-        let description = courses[i].description;
-
-        //Create a tag for the course description
-        let mydesc = document.createElement("p");
-        mydesc.setAttribute("id", "courseDescription"+index);
-        mydesc.innerText = description;
-
-        return mydesc;
+        cnt++;
     }
 }
 
