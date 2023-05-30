d6215afe6c2b07b29e5cd3d570dd8ae524d06e02
JonnyChen2024 <99998530+JonnyChen2024@users.noreply.github.com>
2023-05-29 20:21:41 -0400

    Update addnewpostdata.js

--- addnewpostdata.js
+++ addnewpostdata.js
@@ -1,20 +1,24 @@
+// Combining functions
 async function addcoursesandteachers()
 {
    await addteachers();
    await addcourses();
 }
 
+// Adds teachers to datalist dropdowns in newpost.html
 async function addteachers()
 {
+    // Fetches information from json file
     console.log("Trying to fetch teacher info");
     let response = await fetch("https://raw.githubusercontent.com/E-Fish/CompSci590Proj/main/static/teachers.json");
 
-    //change the link to our mongoDB server when we have it set up
+    // Adds all data to "teachers" variable
     let teachers = await response.json();
  
-    //Get the datalist
+    //Create the datalist
     let datalist = document.getElementById("teacher");
    
+    // Loops through the teachers and adds them to the datalist
     for (let i in teachers) {
         let first = teachers[i].first;
         let last = teachers[i].last;
@@ -25,17 +29,20 @@ async function addteachers()
     }
 } //End addteachers
 
+// Adds courses to datalist dropdowns in newpost.html
 async function addcourses()
 {
+    // Fetches information from json file
     console.log("Trying to fetch teacher info");
     let response = await fetch("https://raw.githubusercontent.com/E-Fish/CompSci590Proj/main/static/courses.json");
 
-    //change the link to our mongoDB server when we have it set up
+    //Adds all data to "courses" variable
     let courses = await response.json();
  
-    //Get the datalist
+    //Create the datalist
     let datalist = document.getElementById("coursecode");
    
+    // Loops through all the courses and adds their course code into the datalist
     for (let i in courses) {
         let name = courses[i].name
 
